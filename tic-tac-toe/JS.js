fc2748bba3b1d1223444d65e3198ba000c3f674f
KurtHsu <myohmy10420@gmail.com>
2016-10-14 21:03:52 +0800

    add function to check winner

--- tic-tac-toe/JS.js
+++ tic-tac-toe/JS.js
@@ -25,3 +25,31 @@ function switchTurn () {
   }
   setMessage("It's " + document.turn + "'s move.");
 }
+
+function getBox(number) {
+  return document.getElementById("s" + number).innerText;
+}
+
+function checkRow (a, b, c, move) {
+  var result = false;
+  if(getBox(a) == move && getBox(b) == move && getBox(c) == move) {
+    result = true;
+  }
+  return result;
+}
+
+function checkForWinner(move) {
+  var result = false;
+  if (checkRow(1, 2, 3, move) ||
+      checkRow(4, 5, 6, move) ||
+      checkRow(7, 8, 9, move) ||
+      checkRow(1, 4, 7, move) ||
+      checkRow(2, 5, 8, move) ||
+      checkRow(3, 6, 9, move) ||
+      checkRow(1, 5, 9, move) ||
+      checkRow(3, 5, 7, move) ||
+     ) {
+    result = true;
+  }
+  return result;
+}
