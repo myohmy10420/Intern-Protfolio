234b4a60085db18c88b0dec06985c10d0e62a302
KurtHsu <myohmy10420@gmail.com>
2016-10-17 20:30:59 +0800

    create onkeydown & up event

--- shooting-game/JS.js
+++ shooting-game/JS.js
@@ -3,10 +3,22 @@ hero.element = 'hero';
 hero.x = 250;
 hero.y = 460;
 
+function toggleKey(keyCode, isPressed) {
+	console.log(keyCode);
+}
+
 function setPosition(sprite) {
 	var e = document.getElementById(sprite.element);
 	e.style.left = sprite.x + 'px';
 	e.style.top = sprite.y + 'px';
 }
+
+document.onkeydown = function (evt) {
+	toggleKey(evt.keyCode, true);
+};
+
+document.onkeyup = function (evt) {
+	toggleKey(evt.keyCode, true);
+};
 
 setPosition(hero);
